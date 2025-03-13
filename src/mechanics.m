9ffb090c271c5b0bc9da5dd847f9ed74b7a6c149
Therkelsen <thomastherkelsen191200@gmail.com>
2025-03-13 20:03:44 +0100

    Mechanics again

--- src/mechanics.m
+++ src/mechanics.m
@@ -2,11 +2,11 @@ clc; clear; close all;
 format compact;
 
 % Force on the Wing Spars
-L = 50.12; % N
-W = 14.73; % N
+FL = 50.12; % N
+FW = 14.73; % N
 
 % % Net Load on both wings
-F_wing = L - W % N
+F_wing = FL - FW % N
 % % Per wing
 F_spar = F_wing/2
 
@@ -25,3 +25,22 @@ E = 10E9;
 
 % Strain
 epsilon = sigma/E
+
+% Length of beam
+L = b/2 % m
+% Line Pressure Load aka
+% Lift per unit length of wing
+p = FL/b % N/m
+
+% Distance from Neutral Axis
+% to Extreme Fibers
+c = h/2 % m
+
+% Moment of Inerta
+I = 1/12*b*h^3 % kg/m^2
+
+% Maximum Moment
+M_max = (L^2*p)/2 % Nm
+
+% Maximum Stress
+sigma_max = (abs(M_max)*c)/I % Pa
